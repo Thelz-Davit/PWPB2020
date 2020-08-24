baaf3c5a8ca1218baca5cd2adbb099528e6492a9
Davitra Eka S <Davitra03@gmail.com>
2020-08-24 20:42:37 +0700

    Constraint & Grid Layout

---
+++ latihanparceable/src/main/java/tugas/davit/latihanparceable/MoveWithDataActivity.java
@@ -0,0 +1,24 @@
+package tugas.davit.latihanparceable;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MoveWithDataActivity extends AppCompatActivity{
+    public static String EXTRA_AGE = "extra_age";
+    public static String EXTRA_NAME = "extra_name";
+    private TextView tvDataReceived;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+        tvDataReceived = (TextView) findViewById(R.id.tv_data_received);
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+        String text = "Name : "+name+", Your Age : "+age;
+        tvDataReceived.setText(text);
+    }
+}
