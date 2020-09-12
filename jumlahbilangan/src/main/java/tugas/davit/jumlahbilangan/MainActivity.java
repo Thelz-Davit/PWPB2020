6ed14d1336fbd1c0aa659a1c50df0b1138a6f4b1
Davitra Eka S <Davitra03@gmail.com>
2020-09-12 22:07:46 +0700

    Custom List View

---
+++ jumlahbilangan/src/main/java/tugas/davit/jumlahbilangan/MainActivity.java
@@ -0,0 +1,48 @@
+package tugas.davit.jumlahbilangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText bil1;
+    private EditText bil2;
+    private EditText jumlah;
+    private Button btnhasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUI();
+        initEvent();
+    }
+
+    private void initEvent() {
+        btnhasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hitungJumlah();
+            }
+        });
+    }
+
+    private void hitungJumlah() {
+        int angka1 = Integer.parseInt(bil1.getText().toString());
+        int angka2 = Integer.parseInt(bil2.getText().toString());
+        int total = angka1 + angka2;
+        jumlah.setText(total+"");
+    }
+
+    private void initUI() {
+        bil1 = (EditText) findViewById(R.id.editTextNumber2);
+        bil2 = (EditText) findViewById(R.id.editTextNumber3);
+        jumlah = (EditText) findViewById(R.id.editTextNumber);
+        btnhasil = (Button) findViewById(R.id.buttonhasil);
+    }
+}
